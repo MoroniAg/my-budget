23182e25bf15996daa4ac10552e41aa912338aca
Moroni Aguilera <52982375+MoroniAg@users.noreply.github.com>
2023-12-19 03:33:48 +0000

    add firebase

--- my-budget-ui/src/App.tsx
+++ my-budget-ui/src/App.tsx
@@ -2,8 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLhWI5LSFTHlDktTrgchJisrCs8n-OG8E",
+  authDomain: "my-budget-302fe.firebaseapp.com",
+  projectId: "my-budget-302fe",
+  storageBucket: "my-budget-302fe.appspot.com",
+  messagingSenderId: "235968058030",
+  appId: "1:235968058030:web:829a91f4f97ce81f7f133f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
+  console.log(app)
   const [count, setCount] = useState(0)
 
   return (
