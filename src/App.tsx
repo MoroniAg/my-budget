ebc6e0d4dcc5f0feb34b0ce931d95362a429fd34
MoroniAg <moroniaguilera1993@gmail.com>
2024-01-12 23:06:07 -0500

    M #18 - add dahsboard ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/security/Login';
 import "primereact/resources/themes/lara-light-indigo/theme.css"
 import "primereact/resources/primereact.min.css"
+import 'primeicons/primeicons.css';
 import './App.css'
 import WithAuthentication from './hooks/WithAuthentication';
 import Dashboard from './pages/dashboard/Dashboard';
