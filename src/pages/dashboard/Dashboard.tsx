55d4b34c2ca8b1603103c4ee73bcffd024f4e9e0
MoroniAg <moroniaguilera1993@gmail.com>
2024-01-12 23:06:07 -0500

    M #18 - add dahsboard ui

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import CardValue from '../../components/CardValue'
+import './Dashboard.css'
 
 export default function Dashboard() {
   return (
-    <div className="flex">
-       <CardValue name='Categorias' icon=''></CardValue>
+    <div className="grid">
+      <div className="col-4">
+        <CardValue name='Categorias'
+          icon='pi pi-book'
+          value='5'
+          styleValue='color-ff6066' />
+      </div>
+      <div className="col-4">
+        <CardValue name='Ingresos'
+          icon='pi pi-book'
+          value='1350'
+          styleValue='color-ffa559' />
+      </div>
+      <div className="col-4">
+        <CardValue name='Egresos'
+          icon='pi pi-book'
+          value='900'
+          styleValue='color-ffa559' />
+      </div>
+
 
     </div>
   )
