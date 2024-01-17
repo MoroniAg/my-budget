5aa5fadddb37614f784d06c1b00051a80d8face1
MoroniAg <moroniaguilera1993@gmail.com>
2024-01-16 22:57:23 -0500

    M #18 - add content into header

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,13 +1,40 @@
 import React from 'react'
 import './Layout.css'
+import { Image } from 'primereact/image';
+import { Button } from 'primereact/button';
+import { classNames } from 'primereact/utils';
+
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+
+    const [sidebar, setSidebar] = React.useState(true)
+    const [sidebarClass, setSidebarClass] = React.useState('')
+
+
+
+    const hideSidebar = () => {
+        const status = sidebar
+        setSidebar(!status)
+        setSidebarClass(!status ? 'fadein' : 'fadeout')
+
+    }
+
     return (
         <>
             <div className="block w-screen h-screen text-color">
-                <div className="header h-5rem bg-white ">header</div>
+                <div className="header h-5rem bg-white ">
+                    <div className="flex ml-5 py-2 align-items-center">
+                        <Image id='' src='/src/assets/logo-1.png' alt='logo' width='50'></Image>
+                        <span className='ml-2 tittle'>Mi Presupuesto</span>
+                        <Button className='ml-7 ' label='' icon='pi pi-ellipsis-v' link onClick={hideSidebar}></Button>
+                    </div>
+                </div>
                 <div className="flex body-content m-5 ">
-                    <div className="sidebar border-round-xl bg-white w-15rem p-3 mr-3">sidebar</div>
+
+                    <div className={classNames(sidebarClass, 'sidebar flex border-round-xl bg-white')}  >
+                        <span>sidebar</span>
+                    </div>
+
                     <div className="content ml-3 w-full">
                         {children}
                     </div>
